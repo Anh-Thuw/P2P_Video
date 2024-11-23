a3eb9f9145b1daac0973ece8b5814cd887d7bd71
Thuxinhdep2905 <thuna.22it@vku.udn.vn>
2024-11-23 08:16:33 +0700

    sql

--- doc/Script-6.sql
+++ doc/Script-6.sql
@@ -1,29 +1,28 @@
 CREATE TABLE users (
-    user_id INT PRIMARY KEY,
-    username VARCHAR(255),
-    email VARCHAR(255),
-    password VARCHAR(255),
-    created_at TIMESTAMP,
-    update_at TIMESTAMP
+                       user_id INT AUTO_INCREMENT PRIMARY KEY,
+                       username VARCHAR(255) NOT NULL,
+                       email VARCHAR(255) UNIQUE NOT NULL,
+                       password VARCHAR(255) NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE meets (
-    meet_id INT PRIMARY KEY,
-    host_user_id INT,
-    room_code VARCHAR(255),
-    created_at TIMESTAMP,
-    update_at TIMESTAMP,
-    status INT,
-    FOREIGN KEY (host_user_id) REFERENCES users(user_id)
+                       meet_id INT AUTO_INCREMENT PRIMARY KEY,
+                       host_user_id INT NOT NULL,
+                       room_code VARCHAR(255) UNIQUE NOT NULL,
+                       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                       updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                       status INT NOT NULL DEFAULT 0,
+                       FOREIGN KEY (host_user_id) REFERENCES users(user_id)
 );
 
 CREATE TABLE meet_participants (
-    id INT PRIMARY KEY,
-    meet_id INT,
-    user_id INT,
-    username VARCHAR(255),
-    joined_at TIMESTAMP,
-    left_at TIMESTAMP,
-    FOREIGN KEY (meet_id) REFERENCES meets(meet_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
+                                   id INT AUTO_INCREMENT PRIMARY KEY,
+                                   meet_id INT NOT NULL,
+                                   user_id INT NOT NULL,
+                                   joined_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                   left_at TIMESTAMP DEFAULT NULL,
+                                   FOREIGN KEY (meet_id) REFERENCES meets(meet_id),
+                                   FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
